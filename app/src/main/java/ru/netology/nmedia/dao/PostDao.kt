c0ac3f2e8bee246120a60a7a7ce7f475c0b9b966
Zakharov Roman, AN-34 <ambeadexter@yandex.ru>
2023-04-20 19:01:09 +0700

    Back to previous both build.gradle files - then CRASH was fixed

--- app/src/main/java/ru/netology/nmedia/dao/PostDao.kt
+++ app/src/main/java/ru/netology/nmedia/dao/PostDao.kt
@@ -3,7 +3,7 @@ package ru.netology.nmedia.dao
 import androidx.paging.PagingSource
 import androidx.room.Dao
 import androidx.room.Insert
-import androidx.room.OnConflictStrategy.Companion.REPLACE
+import androidx.room.OnConflictStrategy.REPLACE
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 import ru.netology.nmedia.entity.DraftCopyEntity
